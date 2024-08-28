223ac0a175085f0962192a5cb69e7f3e89325cd5
La Ngoc Hai <langochai11121998@gmail.com>
2024-08-28 09:03:03 +0700

    1st commit

---
+++ react/index.jsx
@@ -0,0 +1,9 @@
+// ReactApp/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import the root React component
+import App from './App';
+
+// Render the React component into the specified DOM element
+ReactDOM.render(<App />, document.getElementById('react-app'));
